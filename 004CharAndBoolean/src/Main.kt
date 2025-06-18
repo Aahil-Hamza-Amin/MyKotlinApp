5ec30c4fd305b629efb9e411ee31962f75ce0fa0
Aahil-Hamza-Amin <aahilhami@gmail.com>
2025-06-19 02:21:56 +0500

    Added Char and Boolean Datatype File

---
+++ 004CharAndBoolean/src/Main.kt
@@ -0,0 +1,13 @@
+fun main(){
+//    8) Char => For Single character or special Symbol
+
+//    val myCharValue: Char = "" => WRONG Way
+//    val myCharValue: Char = 'D' => Redundant/Explicit type
+    val myCharValue  = 'D' // => RIGHT Way
+    println(myCharValue)
+
+//    9) Boolean
+//    val myBooleanValue:Boolean = true => Redundant/Explicit type
+    val myBooleanValue = false // => Right way
+    println(myBooleanValue)
+}
